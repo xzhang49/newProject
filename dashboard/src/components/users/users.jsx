e86381831a032ec07bfa88cdc35041bbe0b58575
xzhang49 <xzhanguh@gmail.com>
2023-06-02 18:42:18 -0500

    updated

--- dashboard/src/components/users/users.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Users = () => {
-  return (
-    <div>users</div>
-  )
-}
-
-export default Users
--- server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
-};
-
-module.exports = generateToken;
