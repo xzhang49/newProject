7ce35882f405ae32e5923b4e9c6404072aa20ad4
xzhang49 <xzhanguh@gmail.com>
2023-06-04 10:31:50 -0500

    updated

---
+++ customer/src/common/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+const Header = () => {
+  return (
+    <Navbar fixed='top' collapseOnSelect expand="lg" bg="#e00529" variant="light">
+      <Container>
+        <Navbar.Brand href="/">PF Delivery</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">首页(Home)</Nav.Link>
+            <Nav.Link href="/">零食(Snacks)</Nav.Link>
+            <Nav.Link href="/">饮料(Drink)</Nav.Link>
+            <Nav.Link href="/">方便食品(Instant)</Nav.Link>
+          </Nav>
+
+          <Nav>
+            <Nav.Link href="/">
+              <i className='fa fa-user fa-lg icon-circle'></i>
+            </Nav.Link>
+            <Nav.Link href="/">
+              <i className='fa fa-cart-shopping fa-lg icon-circle'></i>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
