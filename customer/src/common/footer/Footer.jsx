a64fd60bda9e22294f79276b9c523f212438e2b9
xzhang49 <xzhanguh@gmail.com>
2023-06-09 16:21:09 -0500

    updated

--- customer/src/common/footer/Footer.jsx
+++ customer/src/common/footer/Footer.jsx
@@ -7,35 +7,34 @@ const Footer = () => {
         <div className = "container">
             <div className = "footer-content text-white grid">
                 <div className = "footer-item text-center">
-                    <h6 className = "fs-17 fw-6">Links</h6>
+                    <h6 className = "fs-17 fw-6">我的链接</h6>
                     <ul>
-                        <li><a href = "/aboutus" className = "fs-15">About Us</a></li>
-                        <li><a href = "/contact" className = "fs-15">Contact Us</a></li>
-                        <li><a href = "/blog" className = "fs-15">Blog</a></li>
-                        <li><a href = "faq" className = "fs-15">FAQ's</a></li>
+                        <li><a href = "/aboutus" className = "fs-15">关于我们</a></li>
+                        <li><a href = "/contact" className = "fs-15">联系我们</a></li>
+                        <li><a href = "faq" className = "fs-15">回答问题</a></li>
                     </ul>
                 </div>
 
                 <div className = "footer-item text-center">
-                    <h6 className='fs-17 fw-6'>Policies</h6>
+                    <h6 className='fs-17 fw-6'>政策法规</h6>
                     <ul>
-                        <li><a href = "/terms" className = "fs-15">Terms & Conditions</a></li>
-                        <li><a href = "/cookies" className = "fs-15">Cookies Policy</a></li>
-                        <li><a href = "/policy" className = "fs-15">Data Policy</a></li>
+                        <li><a href = "/terms" className = "fs-15">条款条例</a></li>
+                        <li><a href = "/cookies" className = "fs-15">行业政策</a></li>
+                        <li><a href = "/policy" className = "fs-15">数据法规</a></li>
                     </ul>
                 </div>
 
                 <div className = "footer-item text-center">
-                    <h6 className='fs-17 fw-6'>About Shopping Hub</h6>
+                    <h6 className='fs-17 fw-6'>关于商店</h6>
                     <ul>
-                        <li><a href = "/terms" className = "fs-15">Company Info</a></li>
-                        <li><a href = "/cookies" className = "fs-15">Branches</a></li>
-                        <li><a href = "/policy" className = "fs-15">Store</a></li>
+                        <li><a href = "/terms" className = "fs-15">公司信息</a></li>
+                        <li><a href = "/cookies" className = "fs-15">分支机构</a></li>
+                        <li><a href = "/policy" className = "fs-15">店铺信息</a></li>
                     </ul>
                 </div>
 
                 <div className = "footer-item text-center">
-                    <h6 className='fs-17 fw-6'>Contact</h6>
+                    <h6 className='fs-17 fw-6'>联系我们</h6>
                     <ul>
                         <li>
                             <span><i className = "fas fa-phone"></i></span>
@@ -44,6 +43,10 @@ const Footer = () => {
                         <li>
                             <span><i className = "fas fa-envelope"></i></span>
                             <span className = "fs-15">info@shophub.com</span>
+                        </li>
+                        <li>
+                            <span><i className = "fas fa-house"></i></span>
+                            <span className = "fs-15">Houston, TX 77027</span>
                         </li>
                     </ul>
                 </div>
